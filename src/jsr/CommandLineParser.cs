42492c447b9408e2bddf2fd163966b43182d45d0
Albert Kapitanov <albert.kapitanov@hotmail.com>
2013-02-01 19:05:09 +0400

    jsr2, in progress, broken

--- src/jsr/CommandLineParser.cs
+++ src/jsr/CommandLineParser.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
@@ -36,7 +37,6 @@ namespace JavaScript.Runtime
             private int _index;
 
             public ParserContext(string commandline)
-             //   : this()
             {
                 _commandline = commandline;
                 _index = -1;
@@ -90,12 +90,9 @@ namespace JavaScript.Runtime
                         yield break;
                     }
 
-                    //if (_index == - 1)
+                    if (!MoveNext())
                     {
-                        if (!MoveNext())
-                        {
-                            yield break;
-                        }
+                        yield break;
                     }
 
                     if (Current != '/')
@@ -105,12 +102,18 @@ namespace JavaScript.Runtime
                     }
 
                     char lastCharacter;
-                    var name = ReadUntil(out lastCharacter, ' ', '\t', '=');
-                    
+                    var name = ReadUntil(QuotedValues.Disable, out lastCharacter, ' ', '\t', '=');
+
                     if (lastCharacter == '=')
                     {
-                        var value = ReadUntil(out lastCharacter, ' ', '\t');
-                        yield return new CommandLineParameter(name, value.Substring(1));
+                        if (!MoveNext())
+                        {
+                            // Valueless parameter
+                            yield return new CommandLineParameter(name, string.Empty);
+                        }
+
+                        var value = ReadUntil(QuotedValues.Enable, out lastCharacter, ' ', '\t');
+                        yield return new CommandLineParameter(name, value);
                     }
                     else
                     {
@@ -149,24 +152,167 @@ namespace JavaScript.Runtime
                 }
             }
 
-            private string ReadUntil(out char lastCharacter, params char[] separators)
+            private enum QuotedValues
             {
-                lastCharacter = (char) 0;
+                Disable,
+                Enable
+            }
+
+            private string ReadUntil(QuotedValues quotedValues, out char lastCharacter, params char[] separators)
+            {
+                var count = 0;
+                var isQuoted = false;
+                lastCharacter = (char)0;
                 var buffer = new StringBuilder();
+
                 while (MoveNext())
                 {
-                    if (separators.Contains(Current))
+                    if (quotedValues == QuotedValues.Enable &&
+                        count == 0 &&
+                        Current == '\"')
                     {
-                        lastCharacter = Current;
-                        StepBack();
-                        break;
+                        separators = new[] { '\"' };
+                        isQuoted = true;
                     }
-                    
-                    buffer.Append(Current);
-                }
+                    else
+                    {
+                        if (separators.Contains(Current))
+                        {
+                            lastCharacter = Current;
+                            if (!isQuoted)
+                            {
+                                StepBack();
+                            }
+                            break;
+                        }
 
+                        buffer.Append(Current);
+                    }
+
+                    count++;
+                }
+                
                 return buffer.ToString();
             }
+        }
+    }
+
+    public static class ApplicationDefinitionLoader
+    {
+        
+        public static ApplicationDefinition FromCommandLine(CommandLineParameters commandLine)
+        {
+            string parameter;
+            if (commandLine.RuntimeParameters.TryGetValue("", out parameter))
+            {
+                
+            }
+            var libraryReferences = 
+
+            return new ApplicationDefinition(
+                commandLine.ExecutablePath,
+                GetLibraryReferences(commandLine),
+
+                
+                );
+        }
+
+    }
+
+    public sealed class ApplicationDefinition
+    {
+        private readonly string _executable;
+        private readonly string[] _libraries;
+        private readonly Dictionary<string, CommandLineParameter> _arguments;
+        private readonly ApplicationRuntimeParameters _runtimeParameters;
+
+        public ApplicationDefinition(
+            string executable, 
+            string[] libraries,
+            Dictionary<string, CommandLineParameter> arguments,
+            ApplicationRuntimeParameters runtimeParameters)
+        {
+            _executable = executable;
+            _libraries = libraries;
+            _arguments = arguments;
+            _runtimeParameters = runtimeParameters;
+        }
+
+        public string Executable { get { return _executable; } }
+
+        public string[] Libraries { get { return _libraries; } }
+
+        public Dictionary<string, CommandLineParameter> Arguments { get { return _arguments; } }
+
+        public ApplicationRuntimeParameters RuntimeParameters { get { return _runtimeParameters; } }
+    }
+
+    public sealed class ApplicationProgramParameters
+    {
+        private readonly HashSet<string> _flags;
+        private readonly Dictionary<string, string> _parameters;
+
+        public ApplicationProgramParameters(HashSet<string> flags, Dictionary<string, string> parameters)
+        {
+            _flags = flags;
+            _parameters = parameters;
+        }
+
+        public HashSet<string> Flags { get { return _flags; } }
+
+        public Dictionary<string, string> Parameters { get { return _parameters; } }
+
+        public static ApplicationProgramParameters FromCommandLine(CommandLineParameters commandLine)
+        {
+            return new ApplicationProgramParameters(
+               new HashSet<string>(GetFlagsIterator(commandLine)),
+               /* */
+                );
+        }
+
+        private static IEnumerable<string> GetFlagsIterator(CommandLineParameters commandLine)
+        {
+            return from value in commandLine.ProgramParameters.Values
+                   where !value.HasValue 
+                   select value.Name;
+        }
+    }
+
+    public sealed class ApplicationRuntimeParameters
+    {
+        private static readonly string[] _EmptyLibraryReferences = new string[0];
+
+        private readonly string[] _libraryReferences;
+
+        public ApplicationRuntimeParameters(string[] libraryReferences)
+        {
+            _libraryReferences = libraryReferences;
+        }
+
+        public string[] LibraryReferences { get { return _libraryReferences; } }
+
+        public static ApplicationRuntimeParameters FromCommandLine(CommandLineParameters commandLine)
+        {
+            return new ApplicationRuntimeParameters(
+                GetLibraryReferences(commandLine)
+                );
+        }
+
+        private static string[] GetLibraryReferences(CommandLineParameters commandLine)
+        {
+            CommandLineParameter parameter;
+            if (commandLine.RuntimeParameters.TryGetValue("", out parameter))
+            {
+                if (parameter.HasValue &&
+                    !string.IsNullOrWhiteSpace(parameter.Value))
+                {
+                    var libraryReferences = parameter.Value
+                        .Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+                    return libraryReferences;
+                }
+            }
+
+            return _EmptyLibraryReferences;
         }
     }
 }
---
+++ test/jsr.test/CommandLineParserTest.cs
@@ -0,0 +1,140 @@
+﻿using NUnit.Framework;
+
+namespace JavaScript.Runtime.Test
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    public class CommandLineParserTest
+    {
+        [Test]
+        [TestCase("")]
+        [TestCase("   ")]
+        public void Parse_empty_string(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.IsNullOrEmpty(result.ExecutablePath);
+            Assert.IsEmpty(result.RuntimeParameters);
+            Assert.IsEmpty(result.ProgramParameters);
+            Assert.IsFalse(result.IsValid);
+        }
+
+        [Test]
+        [TestCase("exe")]
+        [TestCase("exe  ")]
+        [TestCase("  exe")]
+        [TestCase("  exe  ")]
+        public void Parse_exe_path_only(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+            Assert.IsEmpty(result.RuntimeParameters);
+            Assert.IsEmpty(result.ProgramParameters);
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        [TestCase("/x=1 /y=abc /z exe")]
+        [TestCase("/x=1  /y=abc /z exe")]
+        [TestCase(" /x=1 /y=abc /z exe")]
+        [TestCase("/x=1 /y=abc /z  exe")]
+        [TestCase("/x=1 /y=abc /z exe ")]
+        public void Parse_exe_path_and_runtime_params(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+            Assert.AreEqual(3, result.RuntimeParameters.Count);
+            Assert.IsTrue(result.RuntimeParameters["x"].HasValue);
+            Assert.AreEqual("1", result.RuntimeParameters["x"].Value);
+            Assert.IsTrue(result.RuntimeParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.RuntimeParameters["y"].Value);
+            Assert.IsFalse(result.RuntimeParameters["z"].HasValue);
+
+            Assert.IsEmpty(result.ProgramParameters);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        [TestCase("exe /x=1 /y=abc /z")]
+        [TestCase(" exe /x=1 /y=abc /z")]
+        [TestCase("exe  /x=1 /y=abc /z")]
+        [TestCase("exe /x=1 /y=abc  /z")]
+        [TestCase("exe /x=1 /y=abc /z ")]
+        public void Parse_exe_path_and_program_params(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+
+            Assert.AreEqual(3, result.ProgramParameters.Count);
+            Assert.IsTrue(result.ProgramParameters["x"].HasValue);
+            Assert.AreEqual("1", result.ProgramParameters["x"].Value);
+            Assert.IsTrue(result.ProgramParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.ProgramParameters["y"].Value);
+            Assert.IsFalse(result.ProgramParameters["z"].HasValue);
+
+            Assert.IsEmpty(result.RuntimeParameters);
+
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        [TestCase("/x=1 /y=abc /z exe /x=1 /y=abc /z")]
+        [TestCase("/x=1 /y=abc /z  exe /x=1 /y=abc /z")]
+        [TestCase("/x=1 /y=abc /z exe  /x=1 /y=abc /z")]
+        [TestCase(" /x=1 /y=abc /z exe /x=1 /y=abc /z")]
+        [TestCase("/x=1 /y=abc /z exe /x=1 /y=abc /z ")]
+        [TestCase("/x=1 /y=abc  /z exe /x=1 /y=abc /z")]
+        [TestCase("/x=1 /y=abc  /z exe /x=1  /y=abc /z")]
+        public void Parse_exe_path_and_all_params(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+            Assert.AreEqual(3, result.RuntimeParameters.Count);
+            Assert.IsTrue(result.RuntimeParameters["x"].HasValue);
+            Assert.AreEqual("1", result.RuntimeParameters["x"].Value);
+            Assert.IsTrue(result.RuntimeParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.RuntimeParameters["y"].Value);
+            Assert.IsFalse(result.RuntimeParameters["z"].HasValue);
+
+            Assert.AreEqual(3, result.ProgramParameters.Count);
+            Assert.IsTrue(result.ProgramParameters["x"].HasValue);
+            Assert.AreEqual("1", result.ProgramParameters["x"].Value);
+            Assert.IsTrue(result.ProgramParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.ProgramParameters["y"].Value);
+            Assert.IsFalse(result.ProgramParameters["z"].HasValue);
+          
+            Assert.IsTrue(result.IsValid);
+        }
+
+        [Test]
+        [TestCase("/x=\"\" /y=\"abc\" exe /x=\"\" /y=\"abc\"")]
+        public void Parse_exe_path_and_all_params_with_quoted_values(string input)
+        {
+            var result = CommandLineParser.Parse(input);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+
+            Assert.AreEqual("exe", result.ExecutablePath);
+            Assert.AreEqual(2, result.RuntimeParameters.Count);
+            Assert.IsTrue(result.RuntimeParameters["x"].HasValue);
+            Assert.AreEqual("", result.RuntimeParameters["x"].Value);
+            Assert.IsTrue(result.RuntimeParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.RuntimeParameters["y"].Value);
+            
+            Assert.AreEqual(2, result.ProgramParameters.Count);
+            Assert.IsTrue(result.ProgramParameters["x"].HasValue);
+            Assert.AreEqual("", result.ProgramParameters["x"].Value);
+            Assert.IsTrue(result.ProgramParameters["y"].HasValue);
+            Assert.AreEqual("abc", result.ProgramParameters["y"].Value);
+            
+            Assert.IsTrue(result.IsValid);
+        }
+    }
+}
